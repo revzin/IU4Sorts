5b74bf7f9b6d919f175fb7a6e79672d327d1efd2
Grigory Revzin <revzingg@gmail.com>
2013-11-19 00:24:01 +0400

    Initial project files added

---
+++ zinsorts/profile.h
@@ -0,0 +1,13 @@
+// profile.h --- функции для оценок эффективности программного когда.
+// Программист - Ревзин
+
+void	PROF_StartProfile();
+
+float	PROF_GetTimeElasped();
+
+void	PROF_StopProfile();
+
+
+float	PROF_ProfileSort(
+
+	);
