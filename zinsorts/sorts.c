fdd3269ea2a9f8640e9d9763826c54f7e734c385
dFeoktistov <fktstvdns@gmail.com>
2013-11-20 21:47:37 +0400

    Bubble sort.

--- zinsorts/sorts.c
+++ zinsorts/sorts.c
@@ -10,7 +10,37 @@ void SRT_sort_pyramid(void* array, int elem_size, int array_len,
 
 void SRT_sort_bubble(void* array, int elem_size, int array_len, 
 	int (*compare_function)(void* pA, void* pB), int ascending) {
-		// ДЕНИС: Здесь реализация SRT_sort_bubble...
+		int i, j, k;
+		if (ascending==1)
+		{
+			for (i=0; i<array_len; i++)
+			{
+				for (j=array_len-1; j>=i; j--)
+				{
+					if (compare_function(array [j], array [j+1])==1)
+					{
+						k=array[j];
+						array[j]=array[j+1];
+						array[j+1]=k; //swap
+					}
+				}
+			}
+		}
+		else if (ascending==0)
+		{
+			for (i=0; i<array_len; i++)
+			{
+				for (j=array_len-1; j>=i; j--)
+				{
+					if (compare_function(array [j], array [j+1])==0)
+					{
+						k=array[j];
+						array[j]=array[j+1];
+						array[j+1]=k; //swap
+					}
+				}
+			}
+		}
 }
 	/* Количество аргументов немного демотивирует. 
 	По идее, всё, что должны делать эти функции - обрабатывать возвращаемые значения и записывать отсортированный массив?*/
