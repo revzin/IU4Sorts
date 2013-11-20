70fc9adc8d6a62dbf8a49956e7679fc574d748bc
dFeoktistov <fktstvdns@gmail.com>
2013-11-20 20:30:23 +0400

    И снова вопросы.

--- zinsorts/sorts.c
+++ zinsorts/sorts.c
@@ -6,10 +6,11 @@ void SRT_sort_pyramid(void* array, int elem_size, int array_len,
 	int (*compare_function)(void* pA, void* pB), int ascending) {
 		// ДЕНИС: Здесь реализация SRT_sort_pyramid...
 
-
 }
 
 void SRT_sort_bubble(void* array, int elem_size, int array_len, 
 	int (*compare_function)(void* pA, void* pB), int ascending) {
 		// ДЕНИС: Здесь реализация SRT_sort_bubble...
 }
+	/* Количество аргументов немного демотивирует. 
+	По идее, всё, что должны делать эти функции - обрабатывать возвращаемые значения и записывать отсортированный массив?*/
