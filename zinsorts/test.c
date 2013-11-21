6d8754c5a48f9063f433d343ba52f1300d3935f4
Haweye <rayhawkeye@mail.ru>
2013-11-21 22:37:56 +0400

    Paul's work in test.c

--- zinsorts/test.c
+++ zinsorts/test.c
@@ -34,6 +34,45 @@ int TST_TestSort(void (*sort_function)(void* array, int elem_size, int array_len
 	// взяв исходный массив, отсортировав его (используй функцию CompInt из comp.h,
 	// и сверив результат с эталонно отсортированным массивом: см. выше!
 	// Вернуть нужно количество проваленных тестов.
-	return 0;
+	int num = 0;
+	TST_referenceSortRun* need_sort_mass = malloc(sizeof(int) * 10);;
+	
+    for(int i = 0; i < 10; i ++){
+        *need_sort_mass[i] = g_AlreadySortedReference[0][i];        
+    }
+	sort_function(need_sort_mass, sizeof(int), 10, compare_function, 1);
+	bool check = false;
+	for(int i = 0; i < 10; i ++)
+        if (*need_sort_mass[i] != g_AlreadySortedReference[1][i]){
+            check = true;
+            break;                    
+        };        
+    if (check) num += 1;
+    
+    for(int i = 0; i < 10; i ++){
+        *need_sort_mass[i] = g_ReverseSortedReference[0][i];        
+    }
+	sort_function(need_sort_mass, sizeof(int), 10, compare_function, 1);
+	check = false;
+	for(int i = 0; i < 10; i ++)
+        if (*need_sort_mass[i] != g_ReverseSortedReference[1][i]){
+            check = true;
+            break;                    
+        };        
+    if (check) num += 1;
+    
+    for(int i = 0; i < 10; i ++){
+        *need_sort_mass[i] = g_GeneralCase[0][i];        
+    }
+	sort_function(need_sort_mass, sizeof(int), 10, compare_function, 1);
+	check = false;
+	for(int i = 0; i < 10; i ++)
+        if (*need_sort_mass[i] != g_GeneralCase[1][i]){
+            check = true;
+            break;                    
+        };        
+    if (check) num += 1;
+    
+	return num;
 }
 
